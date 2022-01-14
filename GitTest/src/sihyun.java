9388d7cebb987f1105e9e4db8108ec339a34777f
smhrd <smhrd@220.80.88.119>
2022-01-14 12:26:27 +0900

    ㅇㅋㅇㅋ

---
+++ GitTest/src/sihyun.java
@@ -0,0 +1,14 @@
+import com.sun.java_cup.internal.runtime.Scanner;
+
+public class sihyun {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("ㅁㄴㅇㄻㄴㄹㅇㅁㄴㄹㅇㅁㄴㄹㅇㅁㄴㄹㅇㅁㄴㄹㅇㅁㄹㄴㅇㅁㄴㄹㅇ");
+		
+		}
+
+
+	}
+
